95b6419132ee6afd0922c8f8158f58bd0989ca21
Oleksii Pustovit <91pustovit91@gmail.com>
2023-05-01 15:24:49 +0300

    added backend request

---
+++ README.md
@@ -0,0 +1,9 @@
+IQ-TEST
+
+[DEMO-LINK](https://oleksiipustovit.github.io/iq-test-app/)
+
+
+[Макет figma](https://www.figma.com/file/mTNXbFPzU5vaAOlEp0np7d/%D0%A2%D0%B5%D1%81%D1%82-%D0%BD%D0%B0-%D1%83%D1%80%D0%BE%D0%B2%D0%B5%D0%BD%D1%8C-IQ?node-id=0-1&t=OnttWjWTYhO2QZ4B-0)
+
+Дока для запросов:
+https://swapi.dev/api/people/1/ 
--- script/result.js
+++ script/result.js
@@ -14,3 +14,20 @@ function updateTimer() {
     timer.innerHTML = `${minutes}:${seconds}`;
     time--;
 }
+
+document.querySelector('.result__button').addEventListener('click', () => {
+    fetch('https://swapi.dev/api/people/1/ ')
+      .then(response => response.json())
+      .then(data => (
+        document.querySelector('.result__name').innerHTML = data.name,
+        document.querySelector('.result__height').innerHTML = data.height,
+        document.querySelector('.result__mass').innerHTML = data.mass,
+        document.querySelector('.result__hair-color').innerHTML = data.hair_color,
+        document.querySelector('.result__skin-color').innerHTML = data.skin_color,
+        document.querySelector('.result__eye-color').innerHTML = data.eye_color,
+        document.querySelector('.result__birth-year').innerHTML = data.birth_year,
+        document.querySelector('.result__gender').innerHTML = data.gender
+        
+        )
+      );
+});
